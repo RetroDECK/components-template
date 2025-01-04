51efdecaee752473a4098fa76f0e1da9191f310b
XargonWan <XargonWan@gmail.com>
2025-01-04 22:01:08 +0900

    Fixed wrong variable assign

--- automation_tools/update_from_upstream.sh
+++ automation_tools/update_from_upstream.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$UPSTREAM_REPO='com.something.else'
-$TARGET_BRANCH='master'
+UPSTREAM_REPO='com.something.else'
+TARGET_BRANCH='master'
 
 git fetch https://github.com/flathub/"$UPSTREAM_REPO" "$TARGET_BRANCH"  # Fetch the latest changes from the remote master branch
 git merge FETCH_HEAD  # Merge the fetched changes into your current branch
